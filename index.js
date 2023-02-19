bda071af3d248cf40eb094b138da52f9236bd134
Instamedz <111243837+Instamedz@users.noreply.github.com>
2023-02-19 14:03:19 +0530

    updated index.js

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ import cors from "cors"
 import mongoose from "mongoose"
 import { contentrouter } from "./Routes/Rcontent.js"
 import { userRoutes } from "./Routes/Rauth.js"
+import { docRouter } from './Routes/Rdoctor.js';
 
 const app=express()
 app.use(cors())
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
-// app.get("/",(req,res)=>{
-//     res.send("this is a clone app")
-// })
+
 app.use("/content",contentrouter)
 app.use("/auth",userRoutes)
-
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config({path: __dirname+'/.env'});
-//   }
+app.use("/doctor",docRouter)
 
 const PORT=process.env.PORT || 8000
 const CONNECTION_URL=process.env.MONGODB_URL
